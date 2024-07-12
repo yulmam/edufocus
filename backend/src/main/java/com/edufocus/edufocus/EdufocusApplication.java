20de6c4c6f8eda359869d55717b464b98bed27c5
yulmam <yulmam1@naver.com>
2024-07-12 09:55:30 +0900

    Initial commit

---
+++ backend/src/main/java/com/edufocus/edufocus/EdufocusApplication.java
@@ -0,0 +1,13 @@
+package com.edufocus.edufocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdufocusApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/edufocus/edufocus/EdufocusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edufocus.edufocus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
