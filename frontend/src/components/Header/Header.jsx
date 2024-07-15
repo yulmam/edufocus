7fbeb18245a9f1270a989b16c64919585f35ad3e
yulmam <yulmam1@naver.com>
2024-07-15 16:52:53 +0900

    feat: Header 추가

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <ul className={styles.group}>
+          <li>
+            <Link
+              to={'/'}
+              className={styles.logo}
+            >
+              Logo
+            </Link>
+          </li>
+          <li>
+            <Link to={'/'}>전체 강의</Link>
+          </li>
+          <li>
+            <Link to={'/'}>수강중인 강의</Link>
+          </li>
+          <li>
+            <Link to={'/'}>내 학습</Link>
+          </li>
+        </ul>
+        <ul className={styles.group}>
+          <li>
+            <Link to={'/'}>마이페이지</Link>
+          </li>
+          <li>
+            <Link to={'/'}>로그인</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
