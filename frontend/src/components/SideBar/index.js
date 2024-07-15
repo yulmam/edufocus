09d1c43f3fba0cefba53144cbf501ff8437cfc20
yulmam <yulmam1@naver.com>
2024-07-15 10:21:38 +0900

    feat: 사이드바 추가

---
+++ frontend/src/components/SideBar/index.js
@@ -0,0 +1,4 @@
+export { default as SideBar } from './SideBar';
+export { default as SideItem } from './SideItem';
+export { default as SideLink } from './SideLink';
+export { default as SideTitle } from './SideTitle';
