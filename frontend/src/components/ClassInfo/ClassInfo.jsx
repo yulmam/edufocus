1f7716b05db9914b157c61060f080d13ecf1a4da
yulmam <yulmam1@naver.com>
2024-07-16 12:12:08 +0900

    feat: ClassInfo 추가

---
+++ frontend/src/components/ClassInfo/ClassInfo.jsx
@@ -0,0 +1,22 @@
+import styles from './ClassInfo.module.css';
+
+export default function ClassInfo({ classTerm, classTime }) {
+  return (
+    <div className={styles.classInfo}>
+      <div className={styles.title}>수업정보</div>
+      <div className={styles.info}>
+        <div className={styles.textGroup}>
+          <div className={styles.subtitle}>수업 기간</div>
+          <div className={styles.content}>{classTerm}</div>
+        </div>
+        <div className={styles.textGroup}>
+          <div className={styles.subtitle}>수업시간</div>
+          <div className={styles.content}>{classTime}</div>
+        </div>
+      </div>
+      <button className={styles.button}>
+        <div className={styles.buttonText}>수강신청</div>
+      </button>
+    </div>
+  );
+}
