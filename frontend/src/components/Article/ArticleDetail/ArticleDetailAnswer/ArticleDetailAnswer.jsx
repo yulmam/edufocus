cd8a5767f150e56db4095b7e7af41501c94cf9d1
yulmam <yulmam1@naver.com>
2024-07-18 10:18:29 +0900

    feat: ArticleDetailAnswer 받은 답변 출력 기능 추가

--- frontend/src/components/Article/ArticleDetail/ArticleDetailAnswer/ArticleDetailAnswer.jsx
+++ frontend/src/components/Article/ArticleDetail/ArticleDetailAnswer/ArticleDetailAnswer.jsx
@@ -1,13 +1,13 @@
 import styles from './ArticleDetailAnswer.module.css';
 
-export default function ArticleDetailAnswer() {
+export default function ArticleDetailAnswer({ answer }) {
   return (
     <div className={styles.answer}>
       <div className={styles.answerHeader}>
         <div>--</div>
         <div className={styles.author}>선생님의 답변</div>
       </div>
-      <div className={styles.content}>Lorem ipsum dolor sit amet.</div>
+      <div className={styles.content}>{answer}</div>
     </div>
   );
 }
