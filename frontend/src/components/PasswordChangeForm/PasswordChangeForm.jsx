155b16873a148247c10819bff68db913382496ac
yulmam <yulmam1@naver.com>
2024-07-19 17:17:53 +0900

    feat: 패스워드변경 컴포넌트 추가

---
+++ frontend/src/components/PasswordChangeForm/PasswordChangeForm.jsx
@@ -0,0 +1,75 @@
+import { useState, useRef } from 'react';
+import styles from './PasswordChangeForm.module.css';
+
+export default function PasswordChangeForm() {
+  const [errorConfirmMessage, setErrorConfirmMessage] = useState(false);
+  const [errorSameMessage, setErrorSameMessage] = useState(false);
+  const currentPasswordRef = useRef('');
+  const newPasswordRef = useRef('');
+  const confirmPasswordRef = useRef('');
+  const userPassword = '1234';
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const currentPassword = currentPasswordRef.current.value;
+    const newPassword = newPasswordRef.current.value;
+    const confirmPassword = confirmPasswordRef.current.value;
+
+    if (currentPassword === userPassword) {
+      setErrorSameMessage(false);
+    } else {
+      setErrorSameMessage(true);
+    }
+    if (newPassword === confirmPassword) {
+      setErrorConfirmMessage(false);
+    } else {
+      setErrorConfirmMessage(true);
+    }
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={styles.passwordChangeForm}
+    >
+      <span className={styles.passwordText}>비밀번호 변경</span>
+      <div className={styles.inputGroup}>
+        <label htmlFor="currentPassword">현재 비밀번호</label>
+        <input
+          type="password"
+          id="currentPassword"
+          ref={currentPasswordRef}
+          className={errorSameMessage ? styles.inputErrorBox : styles.inputBox}
+          required
+        />
+        {errorSameMessage && <div className={styles.errorMessage}>현재 비밀번호가 일치하지 않습니다.</div>}
+      </div>
+      <div className={styles.inputGroup}>
+        <label htmlFor="newPassword">새 비밀번호</label>
+        <input
+          type="password"
+          id="newPassword"
+          ref={newPasswordRef}
+          className={styles.inputBox}
+          required
+        />
+      </div>
+      <div className={styles.inputGroup}>
+        <label htmlFor="confirmPassword">새 비밀번호 확인</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          ref={confirmPasswordRef}
+          className={errorConfirmMessage ? styles.inputErrorBox : styles.inputBox}
+          required
+        />
+        {errorConfirmMessage && <div className={styles.errorMessage}>비밀번호가 일치하지 않습니다.</div>}
+      </div>
+      <button
+        className={styles.button}
+        type="submit"
+      >
+        <div className={styles.buttonText}>비밀번호 변경</div>
+      </button>
+    </form>
+  );
+}
