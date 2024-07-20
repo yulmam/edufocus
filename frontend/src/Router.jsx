7bc3de90f17df7e3ceb3a0e843d452cf0fc7d096
yulmam <yulmam1@naver.com>
2024-07-21 00:36:37 +0900

    feat: Router에 기본 페이지 추가

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,12 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom';
 import PageLayout from './components/Layout/PageLayout';
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import { HomePage } from './pages/HomePage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 const router = createBrowserRouter([
   {
     path: '',
     element: <PageLayout />,
     errorElement: <NotFoundPage />,
+    children: [
+      {
+        index: true,
+        path: '',
+        element: <HomePage />,
+      },
+    ],
   },
 ]);
 
