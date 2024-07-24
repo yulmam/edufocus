83d42a36f6ec653faab51313a42065a21873a555
yulmam <yulmam1@naver.com>
2024-07-24 16:39:16 +0900

    feat: 공지사항 목록 페이지 API 연결

--- frontend/src/hooks/api/useNotices.js
+++ frontend/src/hooks/api/useNotices.js
@@ -2,12 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 import instance from '../../utils/axios/instance';
 import { API_URL } from '../../constants';
 
-export function useNotices(lectureId) {
-  // TODO: API url 업데이트
-  const response = useQuery({
-    queryKey: ['noticelist', lectureId],
-    queryFn: instance.get(`${API_URL}/notice/${lectureId}`),
+export function useNotices(lectureId, page = 0) {
+  return useQuery({
+    queryKey: ['noticelist', lectureId, page],
+    queryFn: () => instance.get(`${API_URL}/board?lectureId=${lectureId}&category=announcement&pageNo=${page}`),
   });
-
-  return response;
 }
