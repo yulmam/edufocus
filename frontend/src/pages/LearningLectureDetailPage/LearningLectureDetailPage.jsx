f5de2757043fa9cf54058d3535adb8e1331dd9e4
yulmam <yulmam1@naver.com>
2024-07-25 15:12:06 +0900

    feat: lectureLayout, LearningLectureDetailPage에 axios 훅 적용

--- frontend/src/pages/LearningLectureDetailPage/LearningLectureDetailPage.jsx
+++ frontend/src/pages/LearningLectureDetailPage/LearningLectureDetailPage.jsx
@@ -1,22 +1,18 @@
 import ArticlePreview from '../../components/Article/ArticlePreview/ArticlePreview';
 import styles from './LearningLectureDetailPage.module.css';
+import { useNotices } from '../../hooks/api/useNotices';
+import { useParams } from 'react-router-dom';
 
 export default function LearningLectureDetailPage() {
-  const { data: notices } = {
-    data: [
-      { id: 1, title: '공지사항1', sub: '7-12 오전 11:40:57' },
-      { id: 2, title: '공지사하앙2', sub: '7-12 오전 11:40:57' },
-      { id: 3, title: '공지사하앙33', sub: '7-15 오전 11:40:57' },
-      { id: 4, title: '제목만 있는 경우' },
-    ],
-  };
-
+  const { lectureId } = useParams();
+  const { data: noticesData } = useNotices(lectureId);
+  const notices = noticesData?.data;
+  // TODO: QnA 훅 작성 후 사용
   const { data: questions } = {
     data: [
       { id: 2, title: 'Question1', sub: '7-12 오전 11:40:57' },
       { id: 3, title: 'Question2', sub: '7-12 오전 11:40:57' },
       { id: 4, title: '헷갈리는게 있어요', sub: '7-15 오전 11:40:57' },
-      { id: 5, title: '궁금궁금', sub: '7-15 오전 11:40:57' },
     ],
   };
   return (
