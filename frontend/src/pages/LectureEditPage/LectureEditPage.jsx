e02fd5ceda23c84933ff43e31d454c9bc2222dd4
yulmam <yulmam1@naver.com>
2024-08-01 14:44:13 +0900

    feat: LectureEditPage, Lecutre Delete 추가

---
+++ frontend/src/pages/LectureEditPage/LectureEditPage.jsx
@@ -0,0 +1,34 @@
+import { LectureForm } from '../../components/LectureForm';
+import { useLectureEdit } from '../../hooks/api/useLectureEdit';
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
+
+export default function LecutreEditPage() {
+  const { lectureId } = useParams();
+  const location = useLocation();
+  const initialData = location.state.from;
+
+  const navigate = useNavigate();
+  const { lectureEdit } = useLectureEdit();
+
+  const handleSubmit = async (lectureObject) => {
+    const response = await lectureEdit(lectureId, lectureObject)
+      .then((res) => {
+        console.log(res);
+        navigate('..');
+      })
+      .catch((err) => console.log(err));
+    console.log(response?.data);
+  };
+
+  return (
+    <div>
+      <LectureForm
+        initialValues={initialData}
+        onSubmit={handleSubmit}
+        title={'강의 홈'}
+        topic={'강의 수정'}
+        to={'..'}
+      />
+    </div>
+  );
+}
