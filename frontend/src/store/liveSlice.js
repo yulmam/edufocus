e36d62dbe7007aeaefb7a930946edbe9ef39819e
yulmam <yulmam1@naver.com>
2024-08-05 17:18:29 +0900

    feat: 라이브 페이지 권한 체크 추가

--- frontend/src/store/liveSlice.js
+++
@@ -1,6 +0,0 @@
-export const liveSlice = (set) => ({
-  liveToken: null,
-  setLiveToken: (liveToken) => set({ liveToken }),
-  participants: 0,
-  setParticipants: (participants) => set({ participants }),
-});
