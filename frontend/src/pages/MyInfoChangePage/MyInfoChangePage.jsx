601b190cfd1eb4d50a731c520a03959d523610b3
yulmam <yulmam1@naver.com>
2024-08-05 11:15:53 +0900

    fix: put/post 이후 리다이렉트 수정

--- frontend/src/pages/MyInfoChangePage/MyInfoChangePage.jsx
+++ frontend/src/pages/MyInfoChangePage/MyInfoChangePage.jsx
@@ -1,13 +1,15 @@
 import { InfoEditForm } from '../../components/InfoEditForm';
 import { useAuth } from '../../hooks/api/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 export default function MyInfoChangePage() {
+  const navigate = useNavigate();
   const { updateInfo } = useAuth();
 
   const handleSubmit = async (e, username, useremail) => {
     e.preventDefault();
     await updateInfo(username, useremail)
-      .then((res) => console.log(res))
+      .then(() => navigate('/'))
       .catch((err) => console.log(err));
   };
 
