4eef85e7e102ee3a558aec8244d6a4606f31eab3
yulmam <yulmam1@naver.com>
2024-08-06 15:44:51 +0900

    feat: 비밀번호 변경 다른 비밀번호 입력 시 오류 처리 추가

--- frontend/src/pages/PasswordChangePage/PasswordChangePage.jsx
+++ frontend/src/pages/PasswordChangePage/PasswordChangePage.jsx
@@ -1,12 +1,30 @@
 import { PasswordChangeForm } from '../../components/PasswordChangeForm';
 import { useAuth } from '../../hooks/api/useAuth';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function PasswordChangePage() {
   const navigate = useNavigate();
+  const [pwError, setPwError] = useState(false);
   const { updatePassword } = useAuth();
   const handleSubmit = async (currentPw, newPw, newPwCheck) => {
-    await updatePassword(currentPw, newPw, newPwCheck).then(() => navigate('/'));
+    await updatePassword(currentPw, newPw, newPwCheck)
+      .then((res) => {
+        console.log(res);
+        navigate('/');
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        if (err.response.data === 'Current password is incorrect') {
+          console.log('현재 비밀번호 에러');
+          setPwError(true);
+        }
+      });
   };
-  return <PasswordChangeForm onSubmit={handleSubmit} />;
+  return (
+    <PasswordChangeForm
+      onSubmit={handleSubmit}
+      pwError={pwError}
+    />
+  );
 }
