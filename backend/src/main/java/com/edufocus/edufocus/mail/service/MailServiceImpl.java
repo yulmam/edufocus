c77c454f70b7821c90fd31471e9ec94d4eb093cf
yulmam <yulmam1@naver.com>
2024-08-07 17:44:27 +0900

    feat: qna 수정

--- backend/src/main/java/com/edufocus/edufocus/mail/service/MailServiceImpl.java
+++ backend/src/main/java/com/edufocus/edufocus/mail/service/MailServiceImpl.java
@@ -34,7 +34,7 @@ public class MailServiceImpl implements MailService {
         redisUtil.setDataExpire(code, email, 60 * 5L);
 
         SimpleMailMessage mail = createEmail(email, "[EDUFOCUS] 비밀번호 찾기 안내", code);
-        mailSender.send(mail);
+        //mailSender.send(mail);
     }
 
     @Override
