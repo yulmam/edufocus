cd042169eba3484e629325a6c8b26cd29272e3db
yulmam <yulmam1@naver.com>
2024-08-12 10:10:33 +0900

    feat: 퀴즈 시작이후  해당 퀴즈 변경 불가

--- backend/src/main/java/com/edufocus/edufocus/ws/controller/ChatController.java
+++ backend/src/main/java/com/edufocus/edufocus/ws/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.edufocus.edufocus.ws.controller;
 
 
 import com.edufocus.edufocus.global.constant.RabbitMQConstant;
+import com.edufocus.edufocus.quiz.service.QuizSetService;
 import com.edufocus.edufocus.report.service.ReportService;
 import com.edufocus.edufocus.ws.entity.dto.MessageDto;
 import com.edufocus.edufocus.ws.entity.dto.QuizDto;
@@ -17,22 +18,25 @@ public class ChatController {
 
     RabbitTemplate rabbitTemplate;
     ReportService reportService;
+    QuizSetService quizSetService;
 
-    public ChatController(RabbitTemplate rabbitTemplate, ReportService reportService){
+    public ChatController(RabbitTemplate rabbitTemplate, ReportService reportService, QuizSetService quizSetService) {
         this.rabbitTemplate = rabbitTemplate;
         this.reportService = reportService;
+        this.quizSetService = quizSetService;
     }
 
     @MessageMapping("chat.message.{lectureId}")
-    public void sendMessage(@DestinationVariable long lectureId, MessageDto messageDto){
-       rabbitTemplate.convertAndSend(RabbitMQConstant.CHAT_EXCHANGE.getConstant(),
+    public void sendMessage(@DestinationVariable long lectureId, MessageDto messageDto) {
+        rabbitTemplate.convertAndSend(RabbitMQConstant.CHAT_EXCHANGE.getConstant(),
                 RabbitMQConstant.ROUTING_KEY_PREFIX.getConstant() + lectureId,
                 messageDto);
     }
 
     @MessageMapping("chat.quiz.{lectureId}")
-    public void quizStart(@DestinationVariable long lectureId, QuizDto quizDto){
+    public void quizStart(@DestinationVariable long lectureId, QuizDto quizDto) {
         UUID reportSetId = reportService.initReportSet(lectureId, quizDto.getQuizSetId());
+        quizSetService.updateQuizSetTested(quizDto.getQuizSetId());
 
         quizDto.setReportSetId(reportSetId);
 
